452d14911d8f4e1234ff7162ac42ee848fb9ad08
spacedome <spacedome@users.noreply.github.com>
2025-02-08 21:42:20 -0500

    embedding

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,7 @@ import GHC.Generics
 import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Numeric.LinearAlgebra as NLA
 import Data.Bifunctor (bimap)
+import qualified Numeric.LinearAlgebra.Data as MD
 
 -- Define a data structure to hold tensor metadata
 data TensorMetadata = TensorMetadata
@@ -26,6 +27,8 @@ data SafeTensors = SafeTensors
   { metadata :: KM.KeyMap TensorMetadata
   , binaryData :: BL.ByteString
   } deriving (Show)
+
+
 
 -- Parse tensor metadata from JSON
 parseTensorMetadata :: Value -> Parser TensorMetadata
@@ -74,6 +77,37 @@ byteStringToFloats bs = runGet getFloats bs
 
 data Tensor = T1 (NLA.Vector Float) | T2 (NLA.Matrix Float) deriving (Show)
 
+type TensorMap = KM.KeyMap Tensor
+newtype TokenEmbeddingLayer = TokenEmbeddingLayer [NLA.Vector Float]
+newtype PositionEmbeddingLayer = PositionEmbeddingLayer [NLA.Vector Float]
+data LayerNorm = LayerNorm (NLA.Vector Float) (NLA.Vector Float)
+
+getTELayer :: TensorMap -> TokenEmbeddingLayer
+getTELayer tm = TokenEmbeddingLayer x
+  where x = case KM.lookup (K.fromString "wte.weight") tm of
+              Just (T2 m) -> MD.toRows m
+              _ -> undefined
+
+getPELayer :: TensorMap -> PositionEmbeddingLayer
+getPELayer tm = PositionEmbeddingLayer x
+  where x = case KM.lookup (K.fromString "wpe.weight") tm of
+              Just (T2 m) -> MD.toRows m
+              _ -> undefined
+
+getLayerNorm :: TensorMap -> LayerNorm
+getLayerNorm tm = LayerNorm w b
+  where w = case KM.lookup (K.fromString "h.0.ln_1.weight") tm of
+              Just (T1 v) -> v
+              _ -> undefined
+        b = case KM.lookup (K.fromString "h.0.ln_1.bias") tm of
+              Just (T1 v) -> v
+              _ -> undefined
+
+-- h.0.ln_1.bias
+sizeTensor :: Tensor -> String
+sizeTensor (T1 t) = show (NLA.size t)
+sizeTensor (T2 t) = show (NLA.size t)
+
 bytesToTensor :: BL.ByteString -> TensorMetadata -> Tensor
 bytesToTensor bs meta = case shape meta of
   [] -> undefined
@@ -83,17 +117,38 @@ bytesToTensor bs meta = case shape meta of
   _ -> undefined
   where (startpos, endpos) = bimap fromIntegral fromIntegral (dataOffsets meta)
         dataChunk = byteStringToFloats (BL.drop startpos (BL.take endpos bs))
-        
 
+getTensorMap :: SafeTensors -> TensorMap
+getTensorMap ten = fmap (bytesToTensor (binaryData ten)) (metadata ten)
 
--- Example usage
+run :: Maybe SafeTensors -> Maybe String
+run safeten = do
+  t <- safeten
+  let ten = getTensorMap t
+  let model = constructModel ten
+  let next = forward model 15496
+  return (show next)
+
+data GPTModel = GPTModel {wpe :: PositionEmbeddingLayer, wte :: TokenEmbeddingLayer }
+
+constructModel :: TensorMap -> GPTModel
+constructModel tm =  GPTModel {wpe = pe, wte = te}
+  where
+    pe = getPELayer tm
+    te = getTELayer tm
+
+forward :: GPTModel -> Int -> NLA.Vector Float
+forward model token = (wtew !! token) + (head wpew)
+  where TokenEmbeddingLayer wtew = wte model
+        PositionEmbeddingLayer wpew = wpe model
+
 main :: IO ()
 main = do
   results <- readSafeTensors "model.safetensors"
-  case results of
-    Just ten -> BL.putStr (encodePretty (metadata ten))
+  -- case results of
+  --   Just ten -> BL.putStr (encodePretty (metadata ten))
+  --   Nothing -> putStrLn "error"
+  -- putStrLn ""
+  case run results of
+    Just str -> putStrLn str
     Nothing -> putStrLn "error"
-  putStrLn ""
-  case results of
-    Just ten -> print (fmap (bytesToTensor (binaryData ten)) (metadata ten))
-    Nothing -> putStrLn "error2"
