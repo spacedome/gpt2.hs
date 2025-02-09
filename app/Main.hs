a0b787d6ab9f7ff7700066af01a5057f5058ac2e
spacedome <spacedome@users.noreply.github.com>
2025-02-09 18:07:19 -0500

    format

--- app/Main.hs
+++ app/Main.hs
@@ -18,16 +18,16 @@ import qualified Numeric.LinearAlgebra.Data as MD
 
 -- Define a data structure to hold tensor metadata
 data TensorMetadata = TensorMetadata
-  { dtype :: String
-  , shape :: [Int]
-  , dataOffsets :: (Int, Int)
+  { dtype :: String,
+    shape :: [Int],
+    dataOffsets :: (Int, Int)
   }
   deriving (Show, Generic, FromJSON, ToJSON)
 
 -- Define a data structure to hold the parsed safetensors file
 data SafeTensors = SafeTensors
-  { metadata :: KM.KeyMap TensorMetadata
-  , binaryData :: BL.ByteString
+  { metadata :: KM.KeyMap TensorMetadata,
+    binaryData :: BL.ByteString
   }
   deriving (Show)
 
@@ -39,9 +39,9 @@ parseTensorMetadata = withObject "TensorMetadata" $ \obj -> do
   (i, j) <- obj .: "data_offsets"
   return
     ( TensorMetadata
-        { shape = mshape
-        , dataOffsets = (i, j)
-        , dtype = mdtype
+        { shape = mshape,
+          dataOffsets = (i, j),
+          dtype = mdtype
         }
     )
 
@@ -66,16 +66,16 @@ parseWord64 bs
 
 byteStringToFloats :: BL.ByteString -> [Float]
 byteStringToFloats bs = runGet getFloats bs
- where
-  getFloats :: Get [Float]
-  getFloats = do
-    empty <- isEmpty
-    if empty
-      then return []
-      else do
-        x <- getFloatle
-        xs <- getFloats
-        return (x : xs)
+  where
+    getFloats :: Get [Float]
+    getFloats = do
+      empty <- isEmpty
+      if empty
+        then return []
+        else do
+          x <- getFloatle
+          xs <- getFloats
+          return (x : xs)
 
 bytesToTensor :: BL.ByteString -> TensorMetadata -> Tensor
 bytesToTensor bs meta = case shape meta of
@@ -84,84 +84,88 @@ bytesToTensor bs meta = case shape meta of
   [n, m] -> T2 ((n NLA.>< m) dataChunk)
   [1, 1, n, m] -> T2 ((n NLA.>< m) dataChunk)
   _ -> undefined
- where
-  (startpos, endpos) = bimap fromIntegral fromIntegral (dataOffsets meta)
-  dataChunk = byteStringToFloats (BL.drop startpos (BL.take endpos bs))
+  where
+    (startpos, endpos) = bimap fromIntegral fromIntegral (dataOffsets meta)
+    dataChunk = byteStringToFloats (BL.drop startpos (BL.take endpos bs))
 
 getTensorMap :: SafeTensors -> TensorMap
 getTensorMap ten = fmap (bytesToTensor (binaryData ten)) (metadata ten)
 
 getTELayer :: TensorMap -> TokenEmbeddingLayer
 getTELayer tm = TokenEmbeddingLayer x
- where
-  x = case KM.lookup (K.fromString "wte.weight") tm of
-    Just (T2 m) -> MD.toRows m
-    _ -> undefined
+  where
+    x = case KM.lookup (K.fromString "wte.weight") tm of
+      Just (T2 m) -> MD.toRows m
+      _ -> undefined
 
 getPELayer :: TensorMap -> PositionEmbeddingLayer
 getPELayer tm = PositionEmbeddingLayer x
- where
-  x = case KM.lookup (K.fromString "wpe.weight") tm of
-    Just (T2 m) -> MD.toRows m
-    _ -> undefined
+  where
+    x = case KM.lookup (K.fromString "wpe.weight") tm of
+      Just (T2 m) -> MD.toRows m
+      _ -> undefined
 
 getLayerNorm :: TensorMap -> LayerNorm
 getLayerNorm tm = LayerNorm w b
- where
-  w = case KM.lookup (K.fromString "h.0.ln_1.weight") tm of
-    Just (T1 v) -> v
-    _ -> undefined
-  b = case KM.lookup (K.fromString "h.0.ln_1.bias") tm of
-    Just (T1 v) -> v
-    _ -> undefined
+  where
+    w = case KM.lookup (K.fromString "h.0.ln_1.weight") tm of
+      Just (T1 v) -> v
+      _ -> undefined
+    b = case KM.lookup (K.fromString "h.0.ln_1.bias") tm of
+      Just (T1 v) -> v
+      _ -> undefined
 
 getAttention :: TensorMap -> Attention
 getAttention tm = Attention w b
- where
-  w = case KM.lookup (K.fromString "h.0.attn.c_attn.weight") tm of
-    Just (T2 v) -> v
-    _ -> undefined
-  b = case KM.lookup (K.fromString "h.0.attn.c_attn.bias") tm of
-    Just (T1 v) -> v
-    _ -> undefined
-
+  where
+    w = case KM.lookup (K.fromString "h.0.attn.c_attn.weight") tm of
+      Just (T2 v) -> v
+      _ -> undefined
+    b = case KM.lookup (K.fromString "h.0.attn.c_attn.bias") tm of
+      Just (T1 v) -> v
+      _ -> undefined
 
 data Tensor = T1 (NLA.Vector Float) | T2 (NLA.Matrix Float) deriving (Show)
 
 type TensorMap = KM.KeyMap Tensor
 
 newtype TokenEmbeddingLayer = TokenEmbeddingLayer [NLA.Vector Float]
+
 newtype PositionEmbeddingLayer = PositionEmbeddingLayer [NLA.Vector Float]
+
 data BlockLayer = BlockLayer LayerNorm
+
 data LayerNorm = LayerNorm (NLA.Vector Float) (NLA.Vector Float)
+
 data Attention = Attention (NLA.Matrix Float) (NLA.Vector Float)
+
 data GPTModel = GPTModel {wpe :: PositionEmbeddingLayer, wte :: TokenEmbeddingLayer, ln1 :: LayerNorm, attn :: Attention}
 
 constructModel :: TensorMap -> GPTModel
-constructModel tm = GPTModel{wpe = pe, wte = te, ln1 = le, attn = at}
- where
-  pe = getPELayer tm
-  te = getTELayer tm
-  le = getLayerNorm tm
-  at = getAttention tm
+constructModel tm = GPTModel {wpe = pe, wte = te, ln1 = le, attn = at}
+  where
+    pe = getPELayer tm
+    te = getTELayer tm
+    le = getLayerNorm tm
+    at = getAttention tm
 
 forwardLN :: LayerNorm -> NLA.Vector Float -> NLA.Vector Float
 forwardLN (LayerNorm w b) x = y
- where
-  n = fromIntegral (NLA.size x)
-  mean = NLA.scalar (NLA.sumElements x / n)
-  cent = x - mean
-  varx = NLA.sumElements (cent * cent) / n
-  fact = NLA.scalar (sqrt (varx + 1e-5))
-  y = ((x - mean) / fact) * w + b
+  where
+    n = fromIntegral (NLA.size x)
+    mean = NLA.scalar (NLA.sumElements x / n)
+    cent = x - mean
+    varx = NLA.sumElements (cent * cent) / n
+    fact = NLA.scalar (sqrt (varx + 1e-5))
+    y = ((x - mean) / fact) * w + b
 
 forward :: GPTModel -> Int -> NLA.Vector Float
 forward model token = l
- where
-  TokenEmbeddingLayer wtew = wte model
-  PositionEmbeddingLayer wpew = wpe model
-  emb = (wtew !! token) + head wpew
-  l = forwardLN (ln1 model) emb
+  where
+    TokenEmbeddingLayer wtew = wte model
+    PositionEmbeddingLayer wpew = wpe model
+    emb = (wtew !! token) + head wpew
+    l = forwardLN (ln1 model) emb
 
 run :: Maybe SafeTensors -> Maybe String
 run safeten = do
