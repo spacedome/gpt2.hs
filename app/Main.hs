03fe8a7fe35c98fa7678c7182bc71fdb745d647c
spacedome <spacedome@users.noreply.github.com>
2025-02-12 00:31:15 -0500

    read vocab

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -Wno-unused-binds #-}
 
 import Data.List (maximumBy, sortBy)
-import Data.Ord (comparing, Down(Down))
+import Data.Ord (Down (Down), comparing)
 import Loader
 import Model
-import Numeric.LinearAlgebra (toList, fromList)
-import System.Random (randomRIO)
+import Numeric.LinearAlgebra (fromList, toList)
 import Numeric.Natural
+import System.Random (randomRIO)
 
 topK :: V -> V
 topK v = fromList (map f (toList v))
-  where k = sortBy (comparing Down) (toList v) !! 200
-        f x = if x > k then x / 0.8  else -1/0
+  where
+    k = sortBy (comparing Down) (toList v) !! 200
+    -- here 2 is the "tempurature"
+    f x = if x > k then x / 2 else -1 / 0
 
 -- simple max sampler for testing
 sampleMax :: V -> IO Token
@@ -30,19 +32,14 @@ run :: GPT -> Natural -> [Token] -> IO [Token]
 run model iter tokens = do
   next <- sampleLogits $ last $ forwardModel model tokens
   print next
-  if iter == 0 then
-    return (tokens ++ [next])
-  else
-    run model (iter - 1) (tokens ++ [next])
-
+  if iter == 0
+    then return (tokens ++ [next])
+    else run model (iter - 1) (tokens ++ [next])
 
 main :: IO ()
 main = do
   results <- readModel "model.safetensors"
   let model = case results of Right gpt -> gpt; Left err -> error err
-  -- Tokens for "Hello,"
-  -- 
-  -- let generate = take 100 $ iterate (run model) [39377, 39377, 34703, 25, 18435, 11, 314, 716, 262, 13601, 21114, 6814, 9104]
-  -- generate <- run model 15 [39377, 39377, 34703, 25, 18435, 11, 314, 716, 262, 13601, 21114, 6814, 9104]
+  -- Tokens for "Hello, I am"
   generate <- run model 25 [15496, 11, 314, 716]
   print generate
--- app/Token.hs
+++ app/Token.hs
@@ -1,28 +1,22 @@
 module Token where
 
 import Data.Aeson
-import Data.Aeson.KeyMap (KeyMap)
-import qualified Data.Aeson.KeyMap as KeyMap
 import Data.Aeson.Key (toText)
-import Data.ByteString.Lazy (ByteString)
-import Data.ByteString.Lazy.Char8 (pack)
+import qualified Data.Aeson.KeyMap as KeyMap
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Map as Map
 import qualified Data.Text as T
+import Model
 
-swap :: (a, b) -> (b, a)
-swap (x, y) = (y, x)
+type TokenMap = Map.Map Token String
 
-type StringIntMap = KeyMap Int
-
--- Function to parse JSON and convert to list of tuples
-parseStringIntJSON :: ByteString -> Maybe [(Int, String)]
+parseStringIntJSON :: BL.ByteString -> Maybe TokenMap
 parseStringIntJSON json = do
-    keyMap <- decode json
-    return $ map (\(k, v) -> (v, T.unpack (toText k))) (KeyMap.toList keyMap)
+  keyMap <- decode json
+  let pairings = map (\(k, v) -> (v, T.unpack (toText k))) (KeyMap.toList keyMap)
+  return (Map.fromList pairings)
 
-
-imain :: IO ()
-imain = do
-    let json = "{\"foo\": 42, \"bar\": 99, \"baz\": 123}"
-    case parseStringIntJSON (pack json) of
-        Just pairs -> print pairs
-        Nothing   -> putStrLn "Failed to parse JSON."
+readVocab :: String -> IO (Maybe TokenMap)
+readVocab filePath = do
+  contents <- BL.readFile filePath
+  return (parseStringIntJSON contents)
